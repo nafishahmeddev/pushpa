bed27b8a12fb12c5e882508fa8a4e4497d391f4d
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2024-12-26 21:53:41 +0530

    chore:updated backend and frontend

--- backend/src/db/conn.ts
+++ backend/src/db/conn.ts
@@ -1,8 +1,9 @@
 import consola from "consola";
 import { Sequelize, DataTypes } from "sequelize";
 
-export const sequelize = new Sequelize("pushpa", "master", "12345678", {
-  host: "localhost",
+export const sequelize = new Sequelize(process.env.DB_NAME as string, process.env.DB_USER as string, process.env.DB_PASS as string, {
+  host: process.env.DB_HOST as string,
+  port: Number(process.env.DB_PORT),
   dialect: "mysql",
-  logging : e=>consola.info("[SEQUELIZE]",e)
+  logging: e => consola.info("[SEQUELIZE]", e)
 });
