9930b69e6bef1c2dd4c1e47d0bfd08b13355678c
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2024-12-26 21:05:56 +0530

    chore:updated backend and frontend

--- frontend/src/services/table.ts
+++
@@ -1,4 +0,0 @@
-import { ApiRequest } from "@app/lib/axios"
-import { ITable } from "@app/types/table"
-
-export const getTables = () =>  ApiRequest.get("/tables").then(res=>res.data.result as Array<ITable>);
