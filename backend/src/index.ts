c831539857da8e936d675ae84442b21fbc52aeca
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2024-12-27 17:27:42 +0530

    chore:auth

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,3 @@
-//alias support
-import 'module-alias/register'
-import moduleAlias from 'module-alias'
-
-moduleAlias.addAliases({
-  '@app': __dirname
-})
-
 import express, { Application } from "express";
 import dotenv from "dotenv";
 dotenv.config();
