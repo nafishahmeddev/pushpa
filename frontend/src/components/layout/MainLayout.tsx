581a70aef72dad0dda6bbc328aa4a2dbfae2bbbb
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2024-12-29 17:35:45 +0530

    feat:settings page with categories screen

--- frontend/src/components/layout/MainLayout.tsx
+++ frontend/src/components/layout/MainLayout.tsx
@@ -31,9 +31,9 @@ const mainmenu = [
 
 const backendMenu = [
   {
-    label:"Users",
-    icon:"mynaui:users-group",
-    route: "/users"
+    label: "Users",
+    icon: "mynaui:users-group",
+    route: "/users",
   },
   {
     label: "Settings",
@@ -80,7 +80,7 @@ export default function MainLayout() {
   );
   return (
     <div className="h-dvh grid grid-cols-[250px_1fr] grid-rows-1 bg-gray-100">
-      <div className="bg-white border-e h-full grid grid-rows-[60px_1fr_60px]">
+      <div className="bg-white border-e h-full grid grid-rows-[60px_1fr_70px]">
         <div className="logo italic flex h-full font-bold text-xl text-blue-800 px-4 py-3 items-center font-mono border-b">
           {auth.user.restaurant?.name}
         </div>
@@ -88,25 +88,32 @@ export default function MainLayout() {
           <SideMenuList items={mainmenu} />
           <h3 className="px-4 text-gray-500 mt-3">Back office</h3>
           <SideMenuList items={backendMenu} />
-          <div className="flex-1 items-end">
-            <ul className="py-2 flex flex-col gap-1">
-              <li className="px-2">
-                <a
-                  onClick={()=>AuthApi.logout().then(()=>{
-                    navigate("/");
-                  })}
-                  className="flex py-2 px-3 items-center justify-start gap-2 cursor-pointer transition-all hover:bg-blue-800/15 rounded-full text-red-700"
-                >
-                  <Icon icon="mage:logout" height={20} width={20} />
-                  <span>Logout</span>
-                </a>
-              </li>
-            </ul>
-          </div>
         </ScrollView>
+        <div className="px-2 h-full py-2">
+          <div className="bg-gray-200 rounded-full items-center flex p-0.5 gap-2 pe-2 w-full h-full">
+            <div className="h-full aspect-square rounded-full bg-white border flex items-center justify-center">
+              <Icon icon="prime:user" height={24} width={24} />
+            </div>
+            <div className="flex items-start justify-center flex-col flex-1">
+              <span className="text-sm">{auth.user.name}</span>
+              <span className="text-xs">{auth.user.designation}</span>
+            </div>
+
+            <button
+              className=" text-red-700 px-2 hover:opacity-50"
+              onClick={() => {
+                AuthApi.logout().then(() => {
+                  navigate("/");
+                });
+              }}
+            >
+              <Icon icon="mage:logout" height={20} width={20} />
+            </button>
+          </div>
+        </div>
       </div>
-      <div className="h-full grid grid-rows-[60px_1fr]">
-        <div className="h-full bg-white border-b flex justify-between p-4 py-3 ">
+      <div className="h-full grid grid-rows-[1fr]">
+        {/* <div className="h-full bg-white border-b flex justify-between p-4 py-3 ">
           <div className=" bg-gray-200 rounded-full items-center flex px-3">
             <span className="text-gray-400">
               <Icon icon="tabler:search" height={20} width={20} />
@@ -128,11 +135,11 @@ export default function MainLayout() {
               <span className="text-sm">{auth.user.name}</span>
             </div>
 
-            {/* <span className="text-gray-400">
+            <span className="text-gray-400">
               <Icon icon="formkit:down" height={20} width={20} />
-            </span> */}
+            </span>
           </button>
-        </div>
+        </div> */}
         <div className="h-full overflow-auto">
           <Outlet />
         </div>
