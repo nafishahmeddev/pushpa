5ee79779758df2a08ef209c40ed5bc0ec9412c96
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2024-12-30 21:11:10 +0530

    chore:updated table structure

--- backend/src/db/conn.ts
+++ backend/src/db/conn.ts
@@ -1,5 +1,5 @@
-import consola from "consola";
-import { Sequelize, DataTypes } from "sequelize";
+// import consola from "consola";
+import { Sequelize } from "sequelize";
 
 export const sequelize = new Sequelize(process.env.DB_NAME as string, process.env.DB_USER as string, process.env.DB_PASS as string, {
   host: process.env.DB_HOST as string,
