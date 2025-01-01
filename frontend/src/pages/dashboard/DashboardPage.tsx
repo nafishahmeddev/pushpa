9a5600209e6a8f7711bb551beb998d4f378e1bef
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2025-01-01 10:49:59 +0530

    chore:updated table structure

--- frontend/src/pages/dashboard/DashboardPage.tsx
+++ frontend/src/pages/dashboard/DashboardPage.tsx
@@ -31,20 +31,18 @@ export default function DashboardPage() {
     <div className="h-full p-4 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-3 auto-rows-min">
       <div className="border bg-white rounded-xl h-[215px] flex flex-col gap-2 w-full row-span-2 py-4">
         <p className="px-4">Top Selling Items</p>
-        <ScrollView className="flex-1 overflow-auto">
-          <div className="px-4">
-            <table className="text-sm w-full">
-              <tbody>
-                {result.topSellingItems.map((item, i) => (
-                  <tr key={i}>
-                    <td>{item.name}</td>
-                    <td className="w-0 text-lime-700">{item.count}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </ScrollView>
+        <div className="px-4 flex-1 ">
+          <table className="text-sm w-full">
+            <tbody>
+              {result.topSellingItems.map((item, i) => (
+                <tr key={i}>
+                  <td>{item.name}</td>
+                  <td className="w-0 text-lime-700">{item.count}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
 
       <div className="border bg-white rounded-xl h-[100px] p-4 flex flex-col gap-2 w-full">
