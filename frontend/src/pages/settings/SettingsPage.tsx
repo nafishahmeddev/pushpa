782d9555a80fadf5d39971652bdf722184932e74
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2025-01-02 22:19:31 +0530

    feat:settings page with categories screen

--- frontend/src/pages/settings/SettingsPage.tsx
+++ frontend/src/pages/settings/SettingsPage.tsx
@@ -1,54 +1,45 @@
-import { Icon } from "@iconify/react/dist/iconify.js";
+
+import { SideMenuList } from "@app/components/layout/SideMenuList";
 import { useEffect } from "react";
-import { NavLink, Outlet, useLocation, useNavigate } from "react-router";
-
-const items = [
-  {
-    label: "Categories",
-    icon: "clarity:file-group-line",
-    route: "/settings/categories",
-  },
-
-  {
-    label: "Products",
-    icon: "bytesize:book",
-    route: "/settings/products",
-  },
-
-  {
-    label: "Locations",
-    icon: "hugeicons:floor-plan",
-    route: "/settings/locations",
-  },
-
-  {
-    label: "Tables",
-    icon: "solar:armchair-outline",
-    route: "/settings/tables",
-  },
-];
+import { Outlet, useLocation, useNavigate } from "react-router";
 export default function MenuPage() {
-  const { pathname } = useLocation();
+  const {pathname} = useLocation();
   const navigate = useNavigate();
 
-  useEffect(() => {
-    if (pathname == "/settings") {
+  useEffect(()=>{
+    if(pathname == "/settings"){
       navigate("/settings/categories");
     }
-  }, [pathname]);
+
+  },[pathname])
   return (
-    <div className="h-full grid grid-rows-[auto_1fr]">
-      <div className="bg-white border border-t-0 rounded-b-2xl h-min">
-        <ul className="flex flex-row rounded-2xl overflow-hidden justify-center">
-          {items.map((item, index) => (
-            <li key={`item-${index}`}>
-              <NavLink to={item.route} className={({ isActive }) => `flex py-1 border-y-2 border-transparent px-3 items-center justify-start gap-2 cursor-pointer hover:opacity-50 ${isActive ? "text-lime-600 border-b-lime-600" : "text-lime-950"}`}>
-                <Icon icon={item.icon} height={20} width={20} />
-                <span>{item.label}</span>
-              </NavLink>
-            </li>
-          ))}
-        </ul>
+    <div className="h-full grid grid-cols-[auto_1fr]">
+      <div className="bg-white border-e w-[200px]">
+        <SideMenuList items={[
+          {
+            label:"Categories",
+            icon:"clarity:file-group-line",
+            route: "/settings/categories"
+          },
+
+          {
+            label:"Products",
+            icon:"bytesize:book",
+            route: "/settings/products"
+          },
+
+          {
+            label:"Locations",
+            icon:"hugeicons:floor-plan",
+            route: "/settings/locations"
+          },
+
+          {
+            label:"Tables",
+            icon:"solar:armchair-outline",
+            route: "/settings/tables"
+          },
+        ]}/>
       </div>
       <div className="h-full overflow-auto">
         <Outlet />
