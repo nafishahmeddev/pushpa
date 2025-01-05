f500a73cfeb4daf8d65485014ed377f61a70a128
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2025-01-05 12:50:02 +0530

    chore:implemented tan stack form

--- frontend/src/components/ui/form/button.tsx
+++ frontend/src/components/ui/form/button.tsx
@@ -19,7 +19,7 @@ export default function Button({
   return (
     <button
       {...props}
-      className={`flex gap-2 h-10 px-5 hover:opacity-60 disabled:opacity-60 rounded-xl items-center justify-center ${props.className ?? ""}`}
+      className={`flex text-nowrap gap-2 h-10 px-5 hover:opacity-60 disabled:opacity-60 rounded-xl items-center justify-center ${props.className ?? ""}`}
     >
       {loading ? (
         <Icon icon="eos-icons:three-dots-loading" height={35} width={40} />
