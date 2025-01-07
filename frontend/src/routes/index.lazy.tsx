9afb2f4bc46da95e0645d1383a2e45f2bf67c4a0
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2025-01-07 21:49:10 +0530

    chore:implemented tan stack form

--- frontend/src/routes/index.lazy.tsx
+++ frontend/src/routes/index.lazy.tsx
@@ -29,7 +29,7 @@ function RouteComponent() {
   });
 
   return (
-    <div className="grid grid-rows-[auto_auto] gap-6 p-4">
+    <div className="grid grid-rows-[auto_1fr] gap-6 p-4">
       <div className="bg-white inline-flex w-min rounded-xl border">
         {Object.values(TimeFrame).map((value) => (
           <button
@@ -47,52 +47,56 @@ function RouteComponent() {
       {!isLoading && !!error && <>{error.message}</>}
 
       {!isLoading && !error && result && (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          <div className="border bg-white rounded-2xl h-60 flex flex-col gap-2 w-full row-span-2 py-6  dash-card">
-            <p className="px-5 font-bold">Top Selling Items</p>
-            <div className="px-5 flex-1 ">
-              <table className="w-full">
-                <tbody>
-                  {result.topSellingItems.map((item, i) => (
-                    <tr key={i}>
-                      <td className="py-1">{item.name}</td>
-                      <td className="w-0 text-gray-700 py-1 ">{item.count}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
+        <div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            <div className="border bg-white rounded-2xl h-60 flex flex-col gap-2 w-full row-span-2 py-6  dash-card">
+              <p className="px-5 font-bold">Top Selling Items</p>
+              <div className="px-5 flex-1 ">
+                <table className="w-full">
+                  <tbody>
+                    {result.topSellingItems.map((item, i) => (
+                      <tr key={i}>
+                        <td className="py-1">{item.name}</td>
+                        <td className="w-0 text-gray-700 py-1 ">
+                          {item.count}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
             </div>
-          </div>
 
-          <div className="border bg-white rounded-2xl h-28 p-5 flex flex-col gap-2 w-full dash-card">
-            <p className="font-bold">Orders</p>
-            <span className="text-4xl  text-gray-700">{result.orders}</span>
-          </div>
+            <div className="border bg-white rounded-2xl h-28 p-5 flex flex-col gap-2 w-full dash-card">
+              <p className="font-bold">Orders</p>
+              <span className="text-4xl  text-gray-700">{result.orders}</span>
+            </div>
 
-          <div className="border bg-white rounded-2xl h-28 p-5 flex flex-col gap-2 w-full dash-card">
-            <p className="font-bold">Avg. Order Value</p>
-            <span className="text-4xl  text-gray-700">
-              {Formatter.money(result.averageOrder)}
-            </span>
-          </div>
+            <div className="border bg-white rounded-2xl h-28 p-5 flex flex-col gap-2 w-full dash-card">
+              <p className="font-bold">Avg. Order Value</p>
+              <span className="text-4xl  text-gray-700">
+                {Formatter.money(result.averageOrder)}
+              </span>
+            </div>
 
-          <div className="border bg-white rounded-2xl h-28 p-5 flex flex-col gap-2 w-full dash-card">
-            <p className="font-bold">Revenue</p>
-            <span className="text-4xl  text-gray-700">
-              {Formatter.money(result.revenue)}
-            </span>
-          </div>
-          <div className="border bg-white rounded-2xl h-28 p-5 flex flex-col gap-2 w-full dash-card">
-            <p className="font-bold">Tax</p>
-            <span className="text-4xl  text-gray-700">
-              {Formatter.money(result.tax)}
-            </span>
-          </div>
-          <div className="border bg-white rounded-2xl h-28 p-5 flex flex-col gap-2 w-full dash-card">
-            <p className="font-bold">Net Sales</p>
-            <span className="text-4xl  text-gray-700">
-              {Formatter.money(result.netSales)}
-            </span>
+            <div className="border bg-white rounded-2xl h-28 p-5 flex flex-col gap-2 w-full dash-card">
+              <p className="font-bold">Revenue</p>
+              <span className="text-4xl  text-gray-700">
+                {Formatter.money(result.revenue)}
+              </span>
+            </div>
+            <div className="border bg-white rounded-2xl h-28 p-5 flex flex-col gap-2 w-full dash-card">
+              <p className="font-bold">Tax</p>
+              <span className="text-4xl  text-gray-700">
+                {Formatter.money(result.tax)}
+              </span>
+            </div>
+            <div className="border bg-white rounded-2xl h-28 p-5 flex flex-col gap-2 w-full dash-card">
+              <p className="font-bold">Net Sales</p>
+              <span className="text-4xl  text-gray-700">
+                {Formatter.money(result.netSales)}
+              </span>
+            </div>
           </div>
         </div>
       )}
