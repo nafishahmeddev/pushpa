28795839e5cfdfbaffc16dee0ee9fe78997e0d1c
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2025-01-08 20:03:46 +0530

    chore:updated backend and frontend

--- frontend/src/components/layout/partials/MainNav.tsx
+++ frontend/src/components/layout/partials/MainNav.tsx
@@ -14,7 +14,7 @@ export function MainNav({ items }: MainNavProps) {
   return (
     <>
       <ul
-        className={`flex-1 flex h-full items-stretch md:justify-end fixed top-0 w-[300px] bg-white z-50 flex-col transition-all border-e md:border-e-0 md:w-auto md:static md:bg-transparent md:flex-row md:opacity-100 ${menu.open ? "left-0 opacity-100" : "-left-[300px] opacity-60"}`}
+        className={`flex-1 flex h-full items-stretch md:justify-end fixed top-0 w-[300px] bg-white z-50 md:z-0 flex-col transition-all border-e md:border-e-0 md:w-auto md:static md:bg-transparent md:flex-row md:opacity-100 ${menu.open ? "left-0 opacity-100" : "-left-[300px] opacity-60"}`}
       >
         {items.map((item, index) => (
           <li key={`item-${index}`}>
@@ -31,7 +31,7 @@ export function MainNav({ items }: MainNavProps) {
         ))}
       </ul>
       <div
-        className={`fixed top-0 left-0 bg-white/50 h-full w-full z-20 md:hidden transition-all ${menu.open?"visible":" collapse"}`}
+        className={`fixed top-0 left-0 bg-white/50 h-full w-full z-20 md:hidden transition-all ${menu.open ? "visible" : " collapse"}`}
         onClick={() => setMenu((state) => ({ ...state, open: false }))}
       ></div>
     </>
