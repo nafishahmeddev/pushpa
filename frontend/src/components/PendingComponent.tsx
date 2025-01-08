3213644f7a05fe32d41a98517df2983d27d76837
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2025-01-08 18:07:57 +0530

    chore:updated backend and frontend

--- frontend/src/components/PendingComponent.tsx
+++ frontend/src/components/PendingComponent.tsx
@@ -2,7 +2,11 @@ export default function PendingComponent() {
   return (
     <div className="h-full w-full flex items-center justify-center text-center">
       <div>
-        <img src="/preloader.gif" style={{ width: 80, height: 80 }} />
+        <img
+          src="/preloader.gif"
+          loading="lazy"
+          style={{ width: 80, height: 80 }}
+        />
       </div>
     </div>
   );
