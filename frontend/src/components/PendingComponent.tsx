11a0dbd2cafac07d8f6fc8856734df0f07dbdfe3
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2025-01-08 16:22:03 +0530

    chore:updated backend and frontend

--- frontend/src/components/PendingComponent.tsx
+++ frontend/src/components/PendingComponent.tsx
@@ -1,15 +1,8 @@
-import Lottie from "react-lottie-player";
-import preloader from "../assets/preloader-lottie.json";
 export default function PendingComponent() {
   return (
     <div className="h-full w-full flex items-center justify-center text-center">
       <div>
-        <Lottie
-          loop
-          animationData={preloader}
-          play
-          style={{ width: 80, height: 80 }}
-        />
+        <img src="/preloader.gif" style={{ width: 80, height: 80 }} />
       </div>
     </div>
   );
