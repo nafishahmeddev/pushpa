83a1a62be3feeb25ae0566538f29ea7eb20684f4
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2025-01-08 18:20:51 +0530

    chore:updated backend and frontend

--- frontend/src/components/PendingComponent.tsx
+++ frontend/src/components/PendingComponent.tsx
@@ -1,12 +1,10 @@
+import Spinner from "./ui/Spinner";
+
 export default function PendingComponent() {
   return (
     <div className="h-full w-full flex items-center justify-center text-center">
       <div>
-        <img
-          src="/preloader.gif"
-          loading="lazy"
-          style={{ width: 80, height: 80 }}
-        />
+        <Spinner />
       </div>
     </div>
   );
