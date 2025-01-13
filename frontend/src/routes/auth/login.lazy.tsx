64213c35a78e2e1dd8311f263995471dbbe52f37
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2025-01-13 18:49:23 +0530

    chore: updated react version

--- frontend/src/routes/auth/login.lazy.tsx
+++ frontend/src/routes/auth/login.lazy.tsx
@@ -30,67 +30,76 @@ function RouteComponent() {
     }
   }, [auth]);
   return (
-    <div className="flex items-center justify-center p-4 bg-gray-200 h-screen">
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          e.stopPropagation();
-          form.handleSubmit();
-        }}
-        className="flex-1 bg-white p-6 max-w-[350px] flex flex-col rounded-xl"
-      >
-        <h3 className="text-xl">Login</h3>
-        <div className="flex flex-col gap-4 py-4">
-          <form.Field
-            name="email"
-            children={({ state, handleBlur, handleChange, name }) => (
-              <Input
-                type="email"
-                placeholder="Please enter your email"
-                label="Email"
-                required
-                value={state.value}
-                onChange={(e) => handleChange(e.target.value)}
-                onBlur={handleBlur}
-                name={name}
-                error={state.meta.errors.join(" ")}
-                touched={state.meta.isTouched}
-              />
-            )}
-          />
+    <div className="flex bg-gray-50 h-screen">
+      <div className="flex-1 collapse lg:visible flex items-center justify-center p-9 ">
+        <img src="/login-scene.png" className="max-w-full" />
+      </div>
+      <div className="flex-1 bg-white p-6 lg:max-w-[500px] flex items-center justify-center">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            form.handleSubmit();
+          }}
+          className="flex flex-col flex-1 max-w-[350px]"
+        >
+          <h3 className="text-xl">Login</h3>
+          <div className="flex flex-col gap-5 py-4">
+            <form.Field
+              name="email"
+              children={({ state, handleBlur, handleChange, name }) => (
+                <Input
+                  type="email"
+                  placeholder="Please enter your email"
+                  label="Email"
+                  required
+                  value={state.value}
+                  onChange={(e) => handleChange(e.target.value)}
+                  onBlur={handleBlur}
+                  name={name}
+                  error={state.meta.errors.join(" ")}
+                  touched={state.meta.isTouched}
+                />
+              )}
+            />
 
-          <form.Field
-            name="pass"
-            children={({ state, handleBlur, handleChange, name }) => (
-              <Input
-                type="password"
-                placeholder="Please enter your password"
-                label="Password"
-                required
-                value={state.value}
-                onChange={(e) => handleChange(e.target.value)}
-                onBlur={handleBlur}
-                name={name}
-                error={state.meta.errors.join(" ")}
-                touched={state.meta.isTouched}
-              />
-            )}
-          />
-          <form.Subscribe
-            selector={(state) => [state.canSubmit, state.isSubmitting]}
-            children={([canSubmit, isSubmitting]) => (
-              <Button
-                type="submit"
-                className="bg-lime-600 text-white"
-                disabled={!canSubmit || isSubmitting}
-                loading={isSubmitting}
-              >
-                Sign In
-              </Button>
-            )}
-          />
-        </div>
-      </form>
+            <form.Field
+              name="pass"
+              children={({ state, handleBlur, handleChange, name }) => (
+                <Input
+                  type="password"
+                  placeholder="Please enter your password"
+                  label="Password"
+                  required
+                  value={state.value}
+                  onChange={(e) => handleChange(e.target.value)}
+                  onBlur={handleBlur}
+                  name={name}
+                  error={state.meta.errors.join(" ")}
+                  touched={state.meta.isTouched}
+                />
+              )}
+            />
+            <form.Subscribe
+              selector={(state) => [state.canSubmit, state.isSubmitting]}
+              children={([canSubmit, isSubmitting]) => (
+                <Button
+                  type="submit"
+                  className="bg-lime-600 text-white mt-4"
+                  disabled={!canSubmit || isSubmitting}
+                  loading={isSubmitting}
+                >
+                  Sign In
+                </Button>
+              )}
+            />
+
+            <div className="text-center text-gray-400">
+              <small>© 2025 RestPal. All rights reserved.</small>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
