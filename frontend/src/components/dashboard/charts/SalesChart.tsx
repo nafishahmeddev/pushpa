e60ad8ee8f97979f22357df21faad9fba0cf69d5
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2025-01-14 13:41:33 +0530

    chore:updated file name in uploads

--- frontend/src/components/dashboard/charts/SalesChart.tsx
+++ frontend/src/components/dashboard/charts/SalesChart.tsx
@@ -1,5 +1,31 @@
+import Formatter from "@app/lib/formatter";
 import { ResponsiveContainer, Tooltip, ComposedChart, XAxis, Bar } from "recharts";
+import { TooltipProps } from 'recharts';
+// for recharts v2.1 and above
+import {
+    ValueType,
+    NameType,
+} from 'recharts/types/component/DefaultTooltipContent';
 
+const CustomToolTip = ({ active, payload, label }: TooltipProps<ValueType, NameType>) => {
+    if (!active || !payload) {
+      return null;
+    }
+    return (
+      <div
+        className="bg-white border rounded-lg p-2 text-sm"
+      >
+        <p>
+          <strong>{label}</strong>
+        </p>
+        {payload.map((item, i) => (
+          <p key={i}>
+            Sales: <strong>{Formatter.money(Number(item.value || 0))}</strong>
+          </p>
+        ))}
+      </div>
+    );
+  };
 export default function SalesChart({
   data = [],
 }: {
@@ -9,7 +35,7 @@ export default function SalesChart({
     <ResponsiveContainer width="100%" aspect={5} maxHeight={150}>
       <ComposedChart data={data}>
         <Bar dataKey="value" fill="#65A30D9f" radius={10}/>
-        <Tooltip />
+        <Tooltip content={<CustomToolTip/>}/>
         <XAxis dataKey="name" fontFamily="Fira Sans" fontSize={12} />
       </ComposedChart>
     </ResponsiveContainer>
